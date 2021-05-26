2212ddb988ef16900349094f0779c77cb2365ec8
jhare96 <jhare1996@hotmail.com>
2021-05-26 16:41:57 +0100

    improve step speed

--- apple_picker/applepicker.py
+++ apple_picker/applepicker.py
@@ -9,19 +9,22 @@ class ApplePicker(gym.Env):
     def __init__(self, map_filepath='orchid.npy', num_objects=100, default_reward=0):
         super(ApplePicker, self)
         self.action_space = gym.spaces.Discrete(4)
-        self.grid = (np.load(Path(__file__).parent / f'maps/{map_filepath}') * 255).astype(np.uint8)
-        self.state = self.grid.copy()
+        self._grid = (np.load(Path(__file__).parent / f'maps/{map_filepath}') * 255).astype(np.uint8)
+        self.state = self._grid.copy()
         self.item_locs = {}
         self.nobjects = num_objects
-        self.agent_loc = [0,0]
+        self.agent_loc = self.rand_loc()
         self.reset()
         self.def_reward = default_reward
+        self.max_row = self._grid.shape[0]-1
+        self.max_col = self._grid.shape[1]-1
+        print(self._grid.dtype)
 
     def rand_loc(self):
         while True:
-            x = np.random.randint(0, high=self.grid.shape[0])
-            y = np.random.randint(0, high=self.grid.shape[1])
-            if self.grid[x,y,2] == 0:
+            x = np.random.randint(0, high=self._grid.shape[0])
+            y = np.random.randint(0, high=self._grid.shape[1])
+            if self._grid[x,y,2] == 0:
                 return x,y
 
     def generate_random_locs(self, num_objects):
@@ -34,61 +37,67 @@ class ApplePicker(gym.Env):
                 else:
                     self.item_locs[x][y] = 1
                     objects += 1
+                    self.state[x,y,1] = 255
             else:
                 self.item_locs[x] = {y:1}
                 objects += 1
+                self.state[x,y,1] = 255
     
     def step(self, action):
         if action > 3:
             raise ValueError('action size if 4')
+        row, column = self.agent_loc
         if action == 0: # up
-            new_loc = max(self.agent_loc[0] - 1, 0), self.agent_loc[1]
+            next_agent_loc = [row-1, column]
         if action == 1: # down
-            new_loc = min(self.agent_loc[0] + 1, self.grid.shape[0]-1), self.agent_loc[1]
+            next_agent_loc = [row+1, column]
         if action == 2: # left
-            new_loc = self.agent_loc[0], max(self.agent_loc[1] - 1, 0)
+            next_agent_loc = [row, column-1]
         if action == 3: # right
-            new_loc = self.agent_loc[0], min(self.agent_loc[1] + 1, self.grid.shape[1]-1)
+            next_agent_loc = [row, column+1]
 
-        x,y = new_loc
         reward = self.def_reward
-        self.agent_loc = new_loc if self.grid[x,y,1] == 0 else self.agent_loc
-        try:
-            x,y = self.agent_loc
-            del self.item_locs[x][y]
-            self.item_locs = {k:v for k,v in self.item_locs.items() if len(v)>0}
-            reward = 1
-        except KeyError:
-            pass
-        
-        self.update_state()
+        next_row, next_column = next_agent_loc
+        if (0 <= next_row <= self.max_row) and (0 <= next_column <= self.max_col) and self.state[next_row, next_column, 2] == 0:
+            try:
+                del self.item_locs[next_row][next_column]
+                self.item_locs = {k:v for k,v in self.item_locs.items() if len(v)>0}
+                reward = 1
+            except KeyError:
+                pass
+
+           # self.agent_loc = next_agent_loc
+            self.update_state(next_agent_loc)
+
         info = {}
         done = True if len(self.item_locs) == 0 else False
         return self.state, reward, done, info
         
-    def update_state(self):
-        self.state = self.grid.copy()
-        x,y = self.agent_loc
-        self.state[x,y,0] = 255
-        for row, cols in self.item_locs.items():
-            for col in list(cols.keys()):
-                self.state[row, col, 1] = 255
+    def update_state(self, next_agent_loc):
+        row, column = self.agent_loc
+        next_row, next_column = next_agent_loc
+        self.state[row, column] = self._grid[row, column]
+        self.state[next_row, next_column, 0] = 255
+        self.agent_loc = next_agent_loc
     
     def reset(self):
+        self.state = self._grid.copy()
         self.item_locs = {}
-        self.agent_loc = self.rand_loc()
+        agent_loc = self.rand_loc()
         self.generate_random_locs(self.nobjects)
-        self.update_state()
+        self.update_state(agent_loc)
         return self.state
 
 
 
 class ApplePickerDeterministic(ApplePicker):
-    def __init__(self, num_objects=20, default_reward=0):
+    def __init__(self, num_objects=20, default_reward=0, map_filepath='orchid.npy'):
         super(ApplePicker, self)
         self.action_space = gym.spaces.Discrete(4)
-        self.grid = (np.load(Path(__file__).parent / 'grid.npy') * 255).astype(np.uint8)
-        self.state = self.grid.copy()
+        self._grid = (np.load(Path(__file__).parent / f'maps/{map_filepath}') * 255).astype(np.uint8)
+        self.max_row = self._grid.shape[0]-1
+        self.max_col = self._grid.shape[1]-1
+        self.state = self._grid.copy()
         self.item_locs = {}
         self.nobjects = num_objects
         self.start_loc = self.rand_loc()
@@ -98,11 +107,17 @@ class ApplePickerDeterministic(ApplePicker):
         self.item_locs_master = copy.deepcopy(self.item_locs)
         self.reset()
 
-    
+    def _draw_apples(self):
+        for row, columns in self.item_locs.items():
+            for col in columns:
+                self.state[row, col, 1] = 255
+
     def reset(self):
         self.item_locs = copy.deepcopy(self.item_locs_master)
+        self.state = self._grid.copy()
+        self._draw_apples()
         self.agent_loc = self.start_loc
-        self.update_state()
+        self.update_state(self.start_loc)
         return self.state
 
 
@@ -121,6 +136,11 @@ if __name__ == '__main__':
             print('done')
             break
             
+    plt.figure()
+    plt.imshow(obs)
+    
+
+    obs = env.reset()
     plt.figure()
     plt.imshow(obs)
     plt.show()
